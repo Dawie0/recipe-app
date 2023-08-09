d17666cc1f53f08ccdb64dc779ccb697bff4d422
Dawid Fourie <dawief4@gmail.com>
2023-08-09 12:06:20 -0700

    getting vercel ready

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,23 @@
 /* eslint react/prop-types: 0 */
+import axios from 'axios'
 import './card.css'
 
 const Card = ( { recipe } ) => {
     const recipeID = recipe.id
+
+    const getRecipe = async () => {
+        
+        try {
+          const res = await axios.get("http://localhost:5000/api/recipe-details", {
+            params: { recipeID },
+          });
+          
+          const { data } = res;
+        //   setResponse(data.results);
+        } catch (error) {
+          console.error(error)
+        }
+      }
 
     return (
         <div className="col-xs-12 col-sm-4 col-md-3 m-2 card glass-morph">
